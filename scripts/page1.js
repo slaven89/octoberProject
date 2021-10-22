eda71d5b26a76f53030b97b0dbd995e6613a729b
slaven89 <kslav002@gmail.com>
2021-10-22 00:41:48 -0400

    trying to get button to work

--- scripts/page1.js
+++ scripts/page1.js
@@ -1,3 +1,10 @@
 console.log("the js file is running with the site.");
-const  redButton = document.querySelector(.redButton);
-redButton.AddEventListener
+
+let redButton = document.querySelector(".red-button");
+let allred = document.querySelector(".background");
+
+function colorchange() {
+    allred.style.font-family = Calibri;
+}
+
+redButton.onclick = colorchange;
