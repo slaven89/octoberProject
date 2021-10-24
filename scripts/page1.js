8a41ba41af365eba42e23a14c0a7a43a1f040336
slaven89 <kslav002@gmail.com>
2021-10-23 22:13:39 -0400

    removed function

--- scripts/page1.js
+++ scripts/page1.js
@@ -1,12 +1,8 @@
 console.log("the js file is running with the site.");
 
-function setButton() {
 let redButton = document.querySelector(".red-button");      //triggering TypeOf errror with the variable being set
 let background = document.querySelector(".background");
 
 redButton.addEventListener("click", () => {
     console.log("the button has clicked");
 });
-}
-
-window.onload(setButton());
