d693d1dec91f5ba58348ae4e332a3870e0750ebf
slaven89 <kslav002@gmail.com>
2021-10-23 22:12:19 -0400

    going back

--- scripts/page1.js
+++ scripts/page1.js
@@ -1,8 +1,12 @@
 console.log("the js file is running with the site.");
 
-let redButton = document.querySelector(".red-button");
+function setButton() {
+let redButton = document.querySelector(".red-button");      //triggering TypeOf errror with the variable being set
 let background = document.querySelector(".background");
 
 redButton.addEventListener("click", () => {
     console.log("the button has clicked");
 });
+}
+
+window.onload(setButton());
