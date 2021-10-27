be8269586dac4ee3458c80ade0834c1990aca2bb
slaven89 <kslav002@gmail.com>
2021-10-27 16:38:00 -0400

    trying to set onclick event

--- scripts/page1.js
+++ scripts/page1.js
@@ -1,8 +1,10 @@
 console.log("the js file is running with the site.");
 
-let redButton = document.querySelector(".red-button");      //triggering TypeOf errror with the variable being set
-let background = document.querySelector(".background");
+let redButton = document.getElementsByClassName('.red-button');
+let pageColor = document.getElementsByClassName('.background');
 
-redButton.addEventListener("click", () => {
-    console.log("the button has clicked");
-});
+redButton.onclick = changeColorRed();
+
+function changeColorRed() {
+    console.log("47")
+};
