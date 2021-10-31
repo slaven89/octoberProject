11ce85bd71a9add0e5cb2e7b4caf52bb460c86ac
slaven89 <kslav002@gmail.com>
2021-10-31 16:20:54 -0400

    added all color buttons

--- scripts/page1.js
+++ scripts/page1.js
@@ -1,9 +1,23 @@
 console.log("the js file is running with the site.");
 
 let redButton = document.querySelector('#red-button');
-
-console.log(redButton);
+let blueButton = document.querySelector('#blue-button');
+let yellowButton = document.querySelector('#yellow-button');
+let greenButton = document.querySelector('#green-button');
+let returnButton = document.querySelector('#return-button');
 
 redButton.addEventListener('click', function(){
-    document.body.style.background= 'red';
+    document.body.style.background= "hsl(358, 66%, 67%)";
+})
+blueButton.addEventListener('click', function(){
+    document.body.style.background= "hsl(240, 59%, 70%)";
+})
+yellowButton.addEventListener('click', function() {
+    document.body.style.background= "hsl(57, 82%, 62%)";
+})
+greenButton.addEventListener('click', function() {
+    document.body.style.background= "hsl(99, 81%, 67%)";
+})
+returnButton.addEventListener('click', function() {
+    document.body.style.background= "hsl(0, 66%, 87%)";
 })
